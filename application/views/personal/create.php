42197d85adc8d83189ec33d717c6b750f4671306
grebo87 <grebo87@gmail.com>
2016-05-15 14:07:23 -0430

    se agrego

---
+++ application/views/personal/create.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-md-12">
+        <a href="<?php  echo base_url();?>personal" class="btn btn-default"><i class="fa fa-arrow-circle-left"></i> Regresar</a>
+    </div>
+</div>
+<br>
+
+
+
+    <div class="inner-spacer">
+    	<form action="<?php  echo base_url();?>personal/store" id="form-personal-create"  method="post" accept-charset="utf-8">
+    		<?php include('shared/form-fields-create.php'); ?>
+    	<div class="action">
+	        <input type="submit"  class="btn btn-primary" id="registrar" value="Registrar" >
+	        <a href="<?php  echo base_url();?>personal" class="btn btn-default" >Cancelar</a>                  
+    	</div> 
+    	</form>
+    </div>
