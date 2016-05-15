cef9e4e65d014e9dea7aa615510e498fdc67f2bf
grebo87 <grebo87@gmail.com>
2016-05-15 14:06:33 -0430

    se agrego

---
+++ application/views/personal/partials/domicilio.php
@@ -0,0 +1,22 @@
+<br>
+<table class="table table-striped" id="domicilio">
+    <tbody>
+            <tr>
+            <th colspan="4" style="text-align:center;" align="">Domicilio</th>
+            </tr>
+            <tr>
+                <td><strong>Estado:</strong></td>
+                <td><?php echo $domicilio->estado;?></td>
+                <td><strong>Municipio:</strong></td>
+                <td><?php echo $domicilio->municipio;?></td>
+            </tr>
+
+            <tr>
+                <td><strong>Ciudad:</strong></td>
+                <td><?php echo $domicilio->ciudad;?></td>
+                <td><strong>Calle:</strong></td>
+                <td><?php echo $domicilio->calle;?></td>
+            </tr>
+
+    </tbody>
+</table>
