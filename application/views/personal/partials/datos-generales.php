5b571dec83f8ef6be8e6479c9fab4680c2464792
grebo87 <grebo87@gmail.com>
2016-05-15 14:06:42 -0430

    se agrego

--- application/views/personal/partials/datos-generales.php
+++ application/views/personal/partials/datos-generales.php
@@ -1,39 +1,32 @@
 <table class="table table-striped" id="datos-generales">
 	<tbody>
 		<tr>
+			<th colspan="4" style="text-align:center;" align="">Datos Generales</th>
+		</tr>
+		<tr>
 			<td><strong>Nombre:</strong></td>
 			<td><?php echo $personal->nombre; ?> <?php echo $personal->apellido_paterno; ?> <?php echo $personal->apellido_materno; ?></td>
+			<td><strong>Cedula:</strong></td>
+			<td><?php echo $personal->cedula; ?></td>
+		</tr>
+
+		<tr>
+			<td><strong>Edad:</strong></td>
+			<td><?php echo $edad; ?></td>
 			<td><strong>Sexo:</strong></td>
 			<td><?php echo $personal->sexo; ?></td>
 		</tr>
 
 		<tr>
-			<td><strong>Edad:</strong></td>
-			<td colspan="3"><?php //echo $personal->present()->edad; ?></td>
-		</tr>
-
-		<tr>
 			<td><strong>Fecha de Nacimiento:</strong></td>
-			<td><?php echo $personal->fecha_nacimiento; ?></td>
+			<td><?php echo strftime("%d-%m-%Y",strtotime($personal->fecha_nacimiento) );?></td>
 			<td><strong>Lugar de Nacimiento:</strong></td>
 			<td><?php echo $personal->lugar_nacimiento; ?></td>
 		</tr>
 
 		<tr>
-			<td><strong>Nombre del Padre:</strong></td>
-			<td><?php echo $personal->nombre_padre; ?></td>
-			<td><strong>Nombre de la Madre:</strong></td>
-			<td><?php echo $personal->nombre_madre; ?></td>
-		</tr>
-
-		<tr>
 			<td><strong>Estado Civil:</strong></td>
 			<td><?php echo $personal->edo_civil; ?></td>
-			<td><strong>Hijos:</strong></td>
-			<td><?php echo $personal->hijos; ?></td>
-		</tr>
-
-		<tr>
 			<td><strong>E-Mail:</strong></td>
 			<td colspan="3"><?php echo $personal->email; ?></td>
 		</tr>
@@ -53,10 +46,15 @@
 		</tr>
 
 		<tr>
-			<td><strong>Tipo de Sangre:</strong></td>
-			<td><?php echo $personal->tipo_sangre; ?></td>
-			<td><strong>Alergias:</strong></td>
-			<td><?php echo $personal->alergias; ?></td>
+			<td><strong>Numero Seguro Social:</strong></td>
+			<td><?php echo $personal->num_seg_social; ?></td>
+			<td><strong>Grado de Estudio:</strong></td>
+			<td><?php echo $personal->grade_estudio; ?></td>
+		</tr>
+		<tr>
+			<td><strong>Puesto:</strong></td>
+			<td colspan="3"><?php echo $puesto->nombre; ?></td>
+			
 		</tr>
 	</tbody>
 </table>
