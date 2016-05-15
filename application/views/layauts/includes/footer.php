4bd8eb17c8b05170b8267f1a6372d79adc8f724b
grebo87 <grebo87@gmail.com>
2016-05-15 14:11:25 -0430

    update

--- application/views/layauts/includes/footer.php
+++ application/views/layauts/includes/footer.php
@@ -1,3 +1,3 @@
 <p>
-    <span class="fa fa-code"></span> with <span class="fa fa-heart"></span> by <a href="https://github.com/grebo87/test">grebo87</a>
+    <span class="fa fa-code"></span> with <span class="fa fa-heart"></span> by <a href="https://github.com/grebo87/test" target="_blank">grebo87</a>
 </p>
