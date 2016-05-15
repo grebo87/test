54fe1ee33382a1c512df152ffc46df9dee49a72f
grebo87 <grebo87@gmail.com>
2016-05-15 14:06:06 -0430

    se agregaron nuevos metodos

--- application/models/Personal.php
+++ application/models/Personal.php
@@ -38,5 +38,89 @@ class Personal extends CI_Model
     	}
 	}
 
+	function getPuestos()
+	{
+		$query = $this->db->get('puestos');
+
+        //returning
+        if ($query->num_rows() > 0){
+            return $query->result();
+        }
+        else{
+            return null;
+        } 
+	}
+
+	function storePersonal($personal = [])
+	{
+		$this->db->insert('personal', $personal);
+        return $this->db->insert_id();
+	}
+
+	function storeDomicilio($domicilio = [])
+	{
+		$this->db->insert('domicilio', $domicilio);
+        return $this->db->insert_id();
+	}
+
+	function getDomicilio($personal_id='')
+	{
+		$this->db->where('personal_id',$personal_id);
+    	$query = $this->db->get('domicilio',1);
+
+    	//returning
+    	if ($query->num_rows() > 0){
+    		return $query->row();
+    	}
+    	else{
+    		return null;
+    	}
+	}
+
+	function getPuesto($puesto_id='')
+	{
+		$this->db->where('id',$puesto_id);
+    	$query = $this->db->get('puestos',1);
+
+    	//returning
+    	if ($query->num_rows() > 0){
+    		return $query->row();
+    	}
+    	else{
+    		return null;
+    	}
+	}
+
+	function updateDomicilio($domicilio = [])
+	{
+		$this->db->where('id', $domicilio['domicilio_id']);
+		unset($domicilio['domicilio_id']);
+        $this->db->update('domicilio', $domicilio);
+        return $this->db->affected_rows();
+	}
+
+	function updatePuesto($personal = [])
+	{
+		$this->db->where('id', $personal['personal_id']);
+		unset($personal['personal_id']);
+        $this->db->update('personal', $personal);
+        return $this->db->affected_rows();
+    }
+	
+	function updateDatosGenerales($personal = [])
+	{
+		$this->db->where('id', $personal['id']);
+		unset($personal['id']);
+        $this->db->update('personal', $personal);
+        return $this->db->affected_rows();
+	}
+
+	function delete($id='')
+	{
+		$this->db->where('id', $id);		
+        $this->db->delete('personal');
+        return $this->db->affected_rows();
+	}
+
 
 }
